7d4a4f5ca9893b5788b353a5c04399100d3e4fd2
Sea <janneisyvidals@gmail.com>
2015-03-10 18:13:35 -0400

    For Loops

---
+++ Main.java
@@ -0,0 +1,26 @@
+package nyc.c4q.Jvidals1;
+/* Accesscode 2.1
+   March 10th 2015
+   J,Vidals
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* Task to transform a while loop to a for loop
+        int i = 0;
+        while (i <= 100) {
+            i++;
+            System.out.println(i);
+        }
+        */
+
+        for ( int i = 0; i<=100; i++) {
+
+            System.out.println(i);
+        }
+
+    }
+}
